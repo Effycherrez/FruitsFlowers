2d530f9fe80100e0f9c327f9df150d92b651ba9a
Alexa Pinos <alexa_baby_@hotmail.com>
2021-05-10 20:15:21 -0500

    Agregar archivos de proyecto.

---
+++ FruitsFlowers/ventana.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FruitsFlowers
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ventana : ContentPage
+    {
+        public ventana(string usuario, string clave)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+        }
+    }
+}
